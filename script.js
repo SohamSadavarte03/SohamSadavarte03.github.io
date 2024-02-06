0469979780b480b9613fc550da416d96216e79a9
SohamSadavarte03 <143739697+SohamSadavarte03@users.noreply.github.com>
2024-02-06 21:15:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("toggle-mode");
+    const body = document.body;
+
+    toggleButton.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#profile1 h2").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#profile1 img").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#about1 p").classList.add("show");
+    }, 1200);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#about1 h2").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#education1 p").classList.add("show");
+    }, 1200);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#education1 h2").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#achievements1 p").classList.add("show");
+    }, 1200);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#achievements1 h2").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#skills1 p").classList.add("show");
+    }, 1200);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#skills1 h2").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#social-icons h2").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(".about").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(".github-icon").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(".instagram-icon").classList.add("show");
+    }, 1200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("#link").classList.add("show");
+    }, 1200);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("nav").classList.add("show");
+    }, 1200);
+});
